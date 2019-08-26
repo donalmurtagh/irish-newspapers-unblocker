4279082581de8242e3906770eadc216de26afa9e
Dónal Murtagh <domurtag@yahoo.co.uk>
2019-08-26 16:07:36 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,15 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+
+        const blockedPageUrl = tab.url;
+        const queryStringIndex = blockedPageUrl.lastIndexOf("?");
+        //const isIrishTimesPage = new URL(blockedPageUrl).hostname.endsWith("irishtimes.com");
+
+        if (queryStringIndex > -1) {
+            const unblockedPageUrl = blockedPageUrl.substring(0, queryStringIndex);
+            chrome.windows.create({url: unblockedPageUrl, incognito: true});
+        }
+    });
+});
