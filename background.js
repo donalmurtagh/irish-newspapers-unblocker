186fbd51f508f006d83018948e77274d923e1729
Dónal Murtagh <domurtag@yahoo.co.uk>
2019-09-01 00:08:22 +0100

    refactor: remove unnecessary code

--- background.js
+++ background.js
@@ -1,24 +1,21 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function (tab) {
 
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    const blockedPageUrl = tab.url;
+    const queryStringIndex = blockedPageUrl.lastIndexOf("?");
+    const isIrishTimesPage = new URL(blockedPageUrl).hostname.endsWith("irishtimes.com");
 
-        const blockedPageUrl = tab.url;
-        const queryStringIndex = blockedPageUrl.lastIndexOf("?");
-        const isIrishTimesPage = new URL(blockedPageUrl).hostname.endsWith("irishtimes.com");
+    if (isIrishTimesPage && queryStringIndex > -1) {
+        const unblockedPageUrl = blockedPageUrl.substring(0, queryStringIndex);
 
-        if (isIrishTimesPage && queryStringIndex > -1) {
-            const unblockedPageUrl = blockedPageUrl.substring(0, queryStringIndex);
-
-            chrome.browsingData.remove({
-                    "origins": [
-                        "https://www.irishtimes.com",
-                        "http://www.irishtimes.com"
-                    ]
-                },
-                {"cookies": true},
-                () => chrome.tabs.update(tab.id, {url: unblockedPageUrl})
-            );
-        }
-    });
+        chrome.browsingData.remove({
+                "origins": [
+                    "https://www.irishtimes.com",
+                    "http://www.irishtimes.com"
+                ]
+            },
+            {"cookies": true},
+            () => chrome.tabs.update(tab.id, {url: unblockedPageUrl})
+        );
+    }
 });
