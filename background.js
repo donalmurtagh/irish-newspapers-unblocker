5565240a0e96844ea24b07f69499eb9a0d627ff8
Dónal Murtagh <domurtag@yahoo.co.uk>
2019-09-01 01:49:57 +0100

    replace function with ES6 arrow function

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.browserAction.onClicked.addListener(tab => {
 
     const blockedPageUrl = tab.url;
     const queryStringIndex = blockedPageUrl.lastIndexOf("?");
